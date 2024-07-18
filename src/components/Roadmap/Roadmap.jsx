2fdcf91cf265790dcaf918c981c5f6713161c0ac
Gigabull <Gigabull@surveybull.io>
2024-07-18 15:08:39 +0530

    Add Roadmap

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import NavBar from '../Navbar/NavBar'
+import React from "react";
+import NavBar from "../Navbar/NavBar";
+import RoadmapBg from "../../assets/background/FAQsBg.jpg";
+import MapBg from "../../assets/background/MapBg.jpg";
+
+
+import Map from "./Map";
+
 function Roadmap() {
   return (
-    <div>
-      <NavBar visitedPage={"Roadmap"}/>
-      Roadmap</div>
-  )
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${RoadmapBg})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          paddingBottom: "50px",
+        }}
+      >
+        <div className="xl:max-w-[1300px] p-7 mx-auto">
+          <NavBar visitedPage={"Roadmap"} />
+          <div className="py-4 mt-[2rem] md:mt-[3rem]">
+            <div className="text-center font-HelveticaNeueBold text-[28px] md:text-[56px]">
+              RoadMap
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+      style={{
+        backgroundImage: `url(${MapBg})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      >
+        <div className="xl:max-w-[1300px] p-7 mx-auto">
+        <Map />
+      </div>
+      </div>
+    </>
+  );
 }
 
-export default Roadmap
+export default Roadmap;
